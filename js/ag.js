308c814093fa03f9abc5b6e0ae2451caf929f084
evmayrinck <elan.v.may@gmail.com>
2017-03-09 11:03:10 -0800

    Fine tuning map

--- js/ag.js
+++ js/ag.js
@@ -4,13 +4,15 @@
 //API can be found at https://dev.socrata.com/foundry/data.cityofnewyork.us/43hw-uvdj
 var SODAagAPI = "https://data.cityofnewyork.us/resource/43hw-uvdj.json?$$app_token=dNjo0bVaaQvP9GEFDkHGujiRG";
 
-var map = L.map('map').setView([40.7850, -73.9682], 13);
+var map = L.map('map').setView([40.7128, -74.0059], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZXZtYXlyaW5jayIsImEiOiJjajAxaXBja3QwNzhmMndsczk4NnlteG9qIn0.9JNK9Zq_C3I43Uqw4x0KBA', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     accessToken: 'pk.eyJ1IjoiZXZtYXlyaW5jayIsImEiOiJjajAxaXBja3QwNzhmMndsczk4NnlteG9qIn0.9JNK9Zq_C3I43Uqw4x0KBA'
 }).addTo(map);
+
+var nameIn = document.querySelector("#name-input")
 
 fetch(SODAagAPI)
     .then(function(response) {
@@ -29,6 +31,14 @@ fetch(SODAagAPI)
             }
         }
     })
+    .then(nameIn.addEventListener("input", function() {
+        var searchName = nameIn.nodeValue.toLowerCase();
+        var filGals = data.filter(function(galleries) {
+            var galName = gal.name.toLowerCase();
+            return galName.indexOf(searchName) >= 0;
+            return L.marker(gal.latlng).addTo(map);
+        })
+    }))
     .catch(function(err) {
         console.error(err);
         alert(err.message);
