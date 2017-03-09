491eaf79acfda9654c8581ab5d4e35b9f56a1dcd
Audrey Lorberfeld <AudreyLorberfeld@Audreys-MacBook-Pro.local>
2017-03-09 15:14:14 -0500

    Playing around w/embedding Twitter feeds

--- js/transportation.js
+++ js/transportation.js
@@ -1,7 +1,5 @@
+  "use strict";
+
   <script src="http://cdn.jsdelivr.net/alasql/0.3/alasql.min.js"></script>
   
   
-  
-  alasql('SELECT * FROM XLSX("2015Bridges.xlsx",{headers:true})',[],function(data){
-        console.log(data);
-    });
