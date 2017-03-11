ebc8e6942a6563a1dbcc788a9782f5edd364a970
evmayrinck <elan.v.may@gmail.com>
2017-03-10 16:48:21 -0800

    Twitter continues

--- js/js.js
+++ js/js.js
@@ -1,11 +1,5 @@
 "use strict";
-    document.body.addEventListener('mdl-componentupgraded', (event) => {
 
-        if (event.target.className.split(' ').indexOf('mdl-js-layout') < 0) {
-            return
-        }
-
-        // code that loads https://platform.twitter.com/widgets.js goes here
         twttr.widgets.createTimeline(
         {
             sourceType: "profile",
@@ -13,4 +7,4 @@
         },
          document.getElementById("#twitter")
 );
-    });
+
