32527c54d4e889264843d006e306fbbc85903c78
Audrey Lorberfeld <AudreyLorberfeld@Audreys-MacBook-Pro.local>
2017-03-11 13:15:11 -0500

    ugh royally fucking everything up
    don’t mind me

--- js/transportation.js
+++ js/transportation.js
@@ -1,82 +1,69 @@
-  "use strict";
+"use strict";
 
-  var transportJSON = <a href="../Bridges.json"></a>; 
+//SODA Library API: https://dev.socrata.com/foundry/data.cityofnewyork.us/feuq-due4 
+var libraryAPI = "https://data.cityofnewyork.us/resource/feuq-due4.json?$$app_token=RcUPQJWzXQaCJq2SgQgtjkrAL";
 
-  
-function renderBridgeProperties(value, nonNumeric) {
-    //create, populate, and return a new <td> element
+//latitude and longitude of NYC city center
 
-    var td = document.createElement("td");
+var map = L.map('map').setView([40.7128, -74.0059], 13);
 
-    td.textContent = value; 
+//access token: pk.eyJ1IjoiYXVsb3JiZSIsImEiOiJjajA1amE0Mncwank3MnFsczByeTdzYjk1In0.2pqlyLnTotT54ZCoR0rnRA
+//style url: mapbox://styles/mapbox/satellite-v9 
 
-        if (nonNumeric) {
-            td.classList.add("mdl-data-table__cell--non-numeric");
+L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYXVsb3JiZSIsImEiOiJjajA1amE0Mncwank3MnFsczByeTdzYjk1In0.2pqlyLnTotT54ZCoR0rnRA', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([40.7128, -74.0059]).addTo(map)
+  .bindPopup('something nice')
+  .openPopup();
+
+/*var userIn = document.querySelector("#user-input")
+
+var libraries = [];
+
+userIn.addEventListener("input", function() {
+    for (var i = 0; i < libraries.length; i++) {
+	    var rec = libraries[i];
+        var searchIn = userIn.value.toLowerCase();
+        var libraryName = librariesJSON.name.toLowerCase();
+        var libraryAdd = librariesJSON.address.toLowerCase();
+        var libraryWebsite = librariesJSON.URL.toLowerCase();
+
+        if ((libraryName.indexOf(searchIn) >= 0) || (libraryAdd.indexOf(searchIn) >= 0) 
+            || (libraryWebsite.indexOf(searchIn) >=0)) {
+            rec.marker.addTo(map);
+        } else {
+            rec.marker.removeFrom(map);
         };
-
-    return td;
-};
-
-
-
-function renderBridge(bridge) {
-    //create <tr> element
-    var tr = document.createElement("tr");
-
-    //use renderMovieProp() to create <td> elements for
-    //the title, sales, and tickets props and append them
-    //to the <tr> element 
-
-    tr.appendChild(renderBridgeProperties(bridge.feature-carried, true));
-    tr.appendChild(renderBridgeProperties(bridge.verbal-rating));
-  
-
-    //return the <tr> element
-
-    return tr;
-};
-  
-
-
-function renderBridges(bridges) {
-    //clear any existing content within the <tbody> element
-    var tbody = document.querySelector("tbody");
-
-    tbody.textContent="";
-
-    
-    //for each element in the movies array
-    //use renderMovie() to create a full-populated <tr> element
-    
-    for (var i=0; i < bridges.length; i++) {
-        var bridge = bridges[i];
-        tbody.appendChild(renderMovie(bridge));
-    //and append that to the <tbody> elemnet already in the page
-}
-}
-
-
-//render all of the movies
-renderBridges(Bridges);
-
-
-
-var titleInput = document.querySelector("#user-input")
-
-
-titleInput.addEventListener("input", function() {
-
-    var searchString = titleInput.value.toLowerCase();
-
-    var filteredBridges = Bridges.filter(function(bridge) {
-    
-    //return true if this movie should be in the filtered set
-    //or false if not
-
-    var bridgeLower = bridge.feature-carried.toLowerCase();
-
-    return bridgeLower.indexOf(searchString) >= 0;
+    };
 });
 
-    renderBridges(filteredBridges);
-});
+
+function parseAsJSON(response) {
+    return response.json();
+}
+
+function handleError(err) {
+    console.error(err);
+    alert(err.message);
+}
+
+function createMarker(data) {
+        for (var i = 0; i < data.length; i++) {
+            var library = data[i];
+            if (library.the_geom.coordinates != undefined) {
+            var latlng = library.the_geom.coordinates.reverse();
+
+            var marker = L.marker(latlng).addTo(map)
+                .bindPopup("<h4>"+library.name+"</h4>"+
+                "<div>"+library.address+"</div>"+
+                '<div><a href="'+library.URL+'">Website</a></div>');
+
+            records.push({
+                library: lib,
+                marker: marker
+            });
+            }
+        }
+    } **/
